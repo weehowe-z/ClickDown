9ca03c02b96f19a4038f3305ca25f36daca29647
weehowe-z <weehowe.z@gmail.com>
2016-02-22 20:55:38 +0800

    add pdf download

---
+++ download.js
@@ -0,0 +1,24 @@
+// var baseURL = document.URL;
+
+function download()
+{
+	var links = document.getElementsByTagName('a');
+	var i;
+	for (i = 0; i < links.length; ++i) {
+	    var url = links[i].href;
+	    var name = url.split("/").pop();
+	    var ext = name.split(".").pop();//get last element
+	    if (ext == "pdf") downloadURI(url, name);
+	}
+}
+
+
+function downloadURI(uri, name) 
+{
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    link.click();
+}
+
+download();
