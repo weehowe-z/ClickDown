6672820f16dc9a6fefd3479380937927b1bd54a6
weehowe-z <weehowe.z@gmail.com>
2016-02-24 13:36:20 +0800

    add file zip

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript(null, {file:"jquery.min.js"})
+	chrome.tabs.executeScript(null, {file:"jszip.min.js"})
+	chrome.tabs.executeScript(null, {file:"jszip-utils.min.js"})
+	chrome.tabs.executeScript(null, {file:"FileSaver.min.js"})
 	chrome.tabs.executeScript(null, {file:"download.js"})
 });
--- download.js
+++ download.js
@@ -20,5 +20,30 @@ function downloadURI(uri, name)
     link.href = uri;
     link.click();
 }
+
+
+alert(document.URL);
+// var zip = new JSZip();
+// zip.file("Hello.txt", "Hello World\n");
+// var content = zip.generate({type:"blob"});
+// // see FileSaver.js
+// saveAs(content, "example.zip");
+
+
+var zip = new JSZip();
+
+JSZipUtils.getBinaryContent("http://www.cs.sjtu.edu.cn/~gao-xf/computability/Document/Syllabus-ComputabilityTheory.pdf", function (err, data) {
+   if(err) {
+      throw err; // or handle the error
+   }
+   zip.file("Syll.pdf", data, {binary:true});
+   var content = zip.generate({type:"blob"});
+   saveAs(content, "example333.zip");   //FileSaver.js
+   alert("hahaha");
+});
+
+
+
+
 alert(document.URL);
 // download();
